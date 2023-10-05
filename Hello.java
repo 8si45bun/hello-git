c58064e15de11b9c21ce3ce8b64bb34063645810
Junggon Kim <anwjrdmlthf@naver.com>
2023-10-05 04:41:30 +0000

    C4: modify hello.java

--- Hello.java
+++ Hello.java
@@ -3,7 +3,7 @@
  public class Hello {
     public static void main(String[] args) {
           System.out.println("Hello, world from GIT!");
-           
+          System.out.println("Change the world!");
           System.out.println("Hello, world from GIT!");
     }
 
